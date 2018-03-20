1b0eeeb6c4a3d06f182126367ac104a2ba81611b
Rasmus <rsj-kc@outlook.com>
2018-03-20 14:51:35 +0100

    done

--- scripts/create.root.certificate.sh
+++ scripts/create.root.certificate.sh
@@ -1,29 +1,16 @@
 # $1 key filename
 # $2 configuration filename
 # $3 certificate filename
-
-# check for filename
-if [ -z "$1" ]; then
-  echo "Key is not defined, using default:  root/ca/private/ca.key.pem"
-  $1="private/ca.key.pem"
-fi
-# check for configuration filename
-if [ -z "$2" ]; then
-  echo "Configuration is not defined, using default: root/ca/openssl.cfg"
-  $2="openssl.cfg"
-fi
-# check for certificate filename.
-if [ -z "$3" ]; then
-  echo "Name is not defined, using default: root/ca/certs/ca.cert.pem"
-  $3="certs/ca.cert.pem"
-fi
+key=${1:-private/ca.key.pem}
+config=${2:-openssl.cfg}
+filename=${3:-certs/ca.cert.pem}
 cd ../root/ca
 # Create the root certificate
-openssl req -config $2 \
-    -key $1 \
+openssl req -config $config \
+    -key $key \
     -new -509 -days 7300 -sha256 -extensions v3_ca \
-    -out $3
-chmod 444 $3
+    -out $filename
+chmod 444 $filename
 # Verify the root certificate
-openssl x509 -noout -text -in $3
+openssl x509 -noout -text -in $filename
 cd ../../scripts
